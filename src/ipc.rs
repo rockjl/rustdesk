b4ecc13ef65732659806a21873efa3b3ec6badd9
18001396586 <153312043@qq.com>
2024-10-16 17:45:51 +0800

    #

--- src/ipc.rs
+++ src/ipc.rs
@@ -663,6 +663,7 @@ async fn handle(data: Data, stream: &mut Connection) {
 
 pub async fn connect(ms_timeout: u64, postfix: &str) -> ResultType<ConnectionTmpl<ConnClient>> {
     let path = Config::ipc_path(postfix);
+    println!("path:{:#?}", path);
     let client = timeout(ms_timeout, Endpoint::connect(&path)).await??;
     Ok(ConnectionTmpl::new(client))
 }
